e731bf57f89b4e601b3f728e2578400e7f02b483
sabhi128 <sabhiahm12@gmail.com>
2026-02-04 12:24:02 +0500

    Add Vercel deployment configuration for unified frontend/backend

---
+++ api/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from '../backend/config/mongodb.js'
+import connectCloudinary from '../backend/config/cloudinary.js'
+import userRouter from '../backend/routes/userRoute.js'
+import productRouter from '../backend/routes/productRoute.js'
+import cartRouter from '../backend/routes/cartRoute.js'
+import orderRouter from '../backend/routes/orderRoute.js'
+
+const app = express()
+connectDB()
+connectCloudinary()
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
+
+app.get('/api', (req, res) => {
+  res.send("API Working")
+})
+
+export default app
